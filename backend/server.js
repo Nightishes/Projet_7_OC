28c647051f1981d812c4208d32569361740c0864
Nightishes <q.bathany@gmail.com>
2023-06-19 11:51:22 +0200

    Thanks Github for this bug

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,7 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '4000');
 app.set('port', port);
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+
 
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
